8a67c4712dc466e82a7e5be1f945d02edfbeb8b9
Ricardo Bustos <rbo567@gmail.com>
2026-09-16 19:05:16 -0600

    Update run_on_cron_display_on_eink.sh
    Adding mode and photo query parameters

--- run_on_cron_display_on_eink.sh
+++ run_on_cron_display_on_eink.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+mode=$1
+photo_query=$2
+
 # Cambiar explícitamente al directorio del proyecto
 cd /home/pi/projects/display-on-eink-random-photo || exit 1
 
@@ -9,4 +12,4 @@ mkdir -p "$LOG_DIR"
 TIMESTAMP=$(date +%Y%m%d)
 
 # Ejecutar el script desde el directorio correcto
-./display_on_eink_random_info_photo.sh >> "$LOG_DIR/run_${TIMESTAMP}.log" 2>&1
+./display_on_eink_random_info_photo.sh $mode "$photo_query" >> "$LOG_DIR/run_${TIMESTAMP}.log" 2>&1
